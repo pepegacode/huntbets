94bc4816e84496f6aeff080f415627f0560e8891
mahgree <mahgree@iu.edu>
2023-07-03 16:38:31 -0400

    input validation

--- huntbets.py
+++ huntbets.py
@@ -45,25 +45,38 @@ def gameinput():
         name = input("\nEnter player name: ")
         bet = [] #clear bet
         for i in range(int(teamnumber)+1):
-            
-            if i == 1:
-                bet += [input("Enter your %sst bet (on team 1): "%i)]
-            elif i == 2:
-                bet += [input("Enter your %snd bet (on team 2): "%i)]
-            elif i == 3:
-                bet += [input("Enter your %srd bet (on team 3): "%i)]
-            elif i == 0:
-                pass
-            else:
-                bet += [input("Enter your %sth bet (on team %s): " % (i,i))]
-            print(i)
-            
+            while True:
+                if i == 1:
+                    bet += [input("Enter your %sst bet (on team 1): "%i)]
+                elif i == 2:
+                    bet += [input("Enter your %snd bet (on team 2): "%i)]
+                elif i == 3:
+                    bet += [input("Enter your %srd bet (on team 3): "%i)]
+                elif i == 0:
+                    pass
+                else:
+                    bet += [input("Enter your %sth bet (on team %s): " % (i,i))]
+                
+                if i!=0:
+                    try:
+                        bet[i-1]=int(bet[i-1])
+                    except:
+                        print("%s is not a number"%bet[i-1])
+
+                    if type(bet[i-1]) != type(1):
+                        bet=[]
+                        continue
+                    else:
+                        break
+                else:
+                    break
+                
                 
 
-        
+        print(bet)
         for i in range(int(teamnumber)):
             if i == 0:
-                print("Your bet is %s on team %s, " % (bet[i],i+1), end="")
+                print("%s's wager is %s on team %s, " % (name,bet[i],i+1), end="")
             elif i < range(int(teamnumber))[-1]:
                 print("%s on team %s, " % (bet[i],i+1), end="")
             else:
