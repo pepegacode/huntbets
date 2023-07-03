c910fb6de8795bbeb7bf89bfaf4b5617a917c881
mahgree <mahgree@iu.edu>
2023-07-03 14:32:48 -0400

    completely rewrote the whole damn thing

--- huntbets.py
+++ huntbets.py
@@ -2,78 +2,93 @@ import math
 
 #game info input section
 playernumber = input('Enter number of players: ')
-teamnumber = input('Enter number of teams')
+teamnumber = input('Enter number of teams: ')
 wagerlist = {}
 
 for i in range(int(playernumber)):
     name = input("\nEnter player name: ")
     bet = [] #clear bet
-    for i in range(4):
+    for i in range(int(teamnumber)+1):
         if i == 1:
             bet += [int(input("Enter your "+str(i)+"st bet (on team 1): "))]
         elif i == 2:
             bet += [int(input("Enter your "+str(i)+"nd bet (on team 2): "))]
         elif i == 3:
             bet += [int(input("Enter your "+str(i)+"rd bet (on team 3): "))]
-    print("Your bet is "+str(bet[0])+" on Team 1, "+str(bet[1])+" on Team 2, and "+str(bet[2])+" on Team 3") 
+        elif i == 0:
+            pass
+        else:
+            bet += [int(input("Enter your %sth bet (on team %s): " % (i,i)))]
+    
+    for i in range(int(teamnumber)):
+        if i == 0:
+            print("Your bet is %s on team %s, " % (bet[i],i+1), end="")
+        elif i < range(int(teamnumber))[-1]:
+            print("%s on team %s, " % (bet[i],i+1), end="")
+        else:
+            print("and %s on team %s." % (bet[i],i+1))
     wagerlist[name] = bet
 
 print("\n"+str(wagerlist))
 
 #math section
-t1sum = 0
-t2sum = 0
-t3sum = 0
 
-for i in wagerlist:
-    t1sum += wagerlist[i][0]
-    t2sum += wagerlist[i][1]
-    t3sum += wagerlist[i][2]
+pot=0
+ctr=0
+teamsum=0
+sumlist=[]
+for k in wagerlist:
+    for j in wagerlist[k]:
+        if ctr == int(teamnumber):
+            break
+        for i in wagerlist:
+            if ctr == int(teamnumber):
+                break
+            teamsum+=wagerlist[i][ctr]
+        
+        sumlist+=[teamsum]
+        pot+=teamsum
+        ctr+=1
+        teamsum=0
 
-pot = t1sum+t2sum+t3sum
-payout={"Team 1":t1sum,"Team 2":t2sum,"Team 3":t3sum}
-print("\n"+"Team 1 total = "+str(t1sum)+"\nTeam 2 total = "+str(t2sum)+"\nTeam 3 total = "+str(t3sum)+"\nThe pot is "+str(pot))
-for i in payout:
-    pr=(pot-payout[i])/payout[i]
-    payout[i]=round(pr,2)
 
-print("Payout table: "+str(payout))
+
+
+payout=[]
+for i in sumlist:
+    payout+=[round((pot-i)/i,2)]
+
+print("\nPayout table: ")
+for i in range(len(payout)):
+    print("Team %s : %s" % (i+1,payout[i]))
+
 while True:  
-    gwinner = input("\nWho won the game? Enter 'team1','team2', or 'team3': ")
+    gwinner = input("\nWho won the game? Enter the number of the winning team (eg '1'): ")
 
-    if gwinner == "team1":
-        for i in wagerlist:
-            winnings=wagerlist[i][0]+wagerlist[i][0]*payout["Team 1"]
-            print("\n"+str(i)+" wins "+str(winnings))
-            wl=winnings-sum(wagerlist[i])
-            if wl < 0:
-                print("("+str(wl)+")")
-            elif wl > 0:
-                print("(+"+str(wl)+")")
-        break
-    elif gwinner == "team2":
-        for i in wagerlist:
-            winnings=wagerlist[i][1]+wagerlist[i][1]*payout["Team 2"]
-            print("\n"+str(i)+" wins "+str(winnings))
-            wl=winnings-sum(wagerlist[i])
-            if wl < 0:
-                print("("+str(wl)+")")
-            elif wl > 0:
-                print("(+"+str(wl)+")")
-        break
-    elif gwinner == "team3":
-        for i in wagerlist:
-            winnings=wagerlist[i][2]+wagerlist[i][2]*payout["Team 3"]
-            print("\n"+str(i)+" wins "+str(winnings))
-            wl=winnings-sum(wagerlist[i])
-            if wl < 0:
-                print("("+str(wl)+")")
-            elif wl > 0:
-                print("(+"+str(wl)+")")
-        break
-    else:
-        print("invalid input")
+    try:
+        gwinner=int(gwinner)-1
+    except:
+        print("'%s' is not a number asshole" % (gwinner))
         continue
+    
+    if gwinner in range(len(payout)):
+        pass
+    else:
+        print("not a valid team")
+        continue
+
+    if type(gwinner) == type(1):
+    
+        for i in wagerlist:
+            winnings=wagerlist[i][gwinner]+wagerlist[i][gwinner]*payout[gwinner]
+            print("\n"+str(i)+" wins "+str(winnings))
+            wl=winnings-sum(wagerlist[i])
+            if wl < 0:
+                print("("+str(wl)+")")
+            elif wl > 0:
+                print("(+"+str(wl)+")")
+        break
+    
 
 while True:
     exit=input("\n"+"Enter 'exit' to quit: ")
