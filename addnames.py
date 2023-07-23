93090f759db214dacadb6e4276713883408ff661
mahgree <mahgree@iu.edu>
2023-07-22 23:57:42 -0400

    fix

---
+++ addnames.py
@@ -0,0 +1,38 @@
+import keyboard
+import time
+from PIL import Image
+import pygetwindow
+import pyautogui
+import key
+import os
+import keyboard
+import sys
+
+#takes a screenshot of the part of the screen with the username and saves it in the Images folder
+def addImage():
+    if len(os.listdir(key.imagepath)) < 3:
+        path = key.imagepath+"/result%s.png"%(len(os.listdir(key.imagepath))+1)
+    else:
+        print("team full")
+        return "team full"
+    titles = pygetwindow.getAllTitles()
+
+    window = pygetwindow.getWindowsWithTitle('Hunt: Showdown')[0]
+
+    left, top = window.topleft
+    right, bottom = window.bottomright
+    pyautogui.screenshot(path)
+    im = Image.open(path) #this might need adjusting
+    im = im.crop((left+1700,top+920,right-20,bottom-50))
+    im.save(path)
+    #im.show(path)
+    print(left,right,top,bottom)
+    pass
+
+
+while True:
+    if keyboard.is_pressed("right ctrl"):
+            addImage()
+            print("Image added")
+            time.sleep(.25)
+        
--- teamadder.py
+++ teamadder.py
@@ -7,32 +7,10 @@ import pyautogui
 import key
 import os
 import glob
+import keyboard
+import sys
 
 
-#takes a screenshot of the part of the screen with the username and saves it in the Images folder
-def addImage():
-    if len(os.listdir(key.imagepath)) < 3:
-        path = key.imagepath+"/result%s.png"%(len(os.listdir(key.imagepath))+1)
-    #elif len(os.listdir(key.imagepath)) == 1:
-    #    path = key.imagepath+"/result2.png"
-    #elif len(os.listdir(key.imagepath)) == 2:
-    #    path = key.imagepath+"/result3.png"
-    else:
-        print("team full")
-        return "team full"
-    titles = pygetwindow.getAllTitles()
-
-    window = pygetwindow.getWindowsWithTitle('Hunt: Showdown')[0]
-
-    left, top = window.topleft
-    right, bottom = window.bottomright
-    pyautogui.screenshot(path)
-    im = Image.open(path) #this might need adjusting
-    im = im.crop((left+1200,top+800,right,bottom-50))
-    im.save(path)
-    im.show(path)
-    print(left,right,top,bottom)
-    pass
 
 #reads images in the images folder, then returns a list of usernames
 def readImages():
@@ -40,11 +18,19 @@ def readImages():
     scrn = os.listdir(key.imagepath)
     for f in scrn:
         img = numpy.array(Image.open(key.imagepath+'/'+f))
-        plist += pytesseract.image_to_string(img)
-    print(plist)
+        read = pytesseract.image_to_string(img)
+        plist += [read]
+    print("Complete HERE: %s"%plist)
     return plist
     pass
 
-addImage()
-print(os.listdir(key.imagepath))
-readImages()
+#clears the images folder
+def clearImages():
+    files = os.listdir(key.imagepath)
+    for f in files:
+        os.remove(key.imagepath+"/%s"%f)
+
+
+
+
+        
